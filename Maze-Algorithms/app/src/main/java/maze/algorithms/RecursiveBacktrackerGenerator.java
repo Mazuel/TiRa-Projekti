d2bc06a26c1b781a13cfc4f002833258d8f88fbd
Masa Petra <mvpetra@gmail.com>
2021-08-14 01:53:20 +0300

    Added report for week 4

--- Maze-Algorithms/app/src/main/java/maze/algorithms/RecursiveBacktrackerGenerator.java
+++ Maze-Algorithms/app/src/main/java/maze/algorithms/RecursiveBacktrackerGenerator.java
@@ -44,24 +44,35 @@ public class RecursiveBacktrackerGenerator {
         timer.start();
     }
 
+    // Algoritmin toiminta sen jälkeen kun aloitus solu on valittu:
+    // 1. Valitse nykyisen solun satunnainen naapuri, jossa ei ole vierailtu
+    // 2. Jos naapuri löytyi, lisää nykyinen solu pinoon ja poista seinä nykyisen
+    // solun ja naapurin väliltä
+    // 3. Jos naapuria ei löytynyt, otetaan pinosta aikaisemmin vierailtu solu
+    // 4. Toistetaan askelia 1-3 kunnes pino on tyhjä
+
     private void generate() {
         currentCell.setVisited(true);
 
         CellList unvisitedNeighbours = currentCell.getUnvisitedNeighbours(grid);
         Cell nextDirection = null;
         if (unvisitedNeighbours.size() > 0) {
+            // 1
             nextDirection = unvisitedNeighbours.get(randomCellPicker.nextInt(unvisitedNeighbours.size()));
         }
 
         if (nextDirection != null) {
+            // 2
             cellStack.push(currentCell);
             currentCell.removeWall(nextDirection);
             currentCell = nextDirection;
         } else if (cellStack.isNotEmpty()) {
+            // 3
             currentCell.setRecursiveVisit(true);
             currentCell = cellStack.pop();
-            currentCell.setRecursiveVisit(true);
         } else {
+            // 4
+            currentCell.setRecursiveVisit(true);
             Maze.generated = true;
         }
     }
---
+++ Viikkoraportit/Viikkoraportti_4.md
@@ -0,0 +1,15 @@
+## Viikko 4
+Parannettu käyttöliittymää hieman: 
+
+- [x] Käyttäjä voi resetoida näkymän ja ajaa jonkun toisen algorithmin ilman erillistä uudelleenkäynnistystä. Tämä on hieman tyhmästi toteutettu mielestäni, mutta en löytänyt ainakaan vielä parempaa toimivaa ratkaisua
+
+Lisätty:
+
+- [x] Oma pinorakenne Cell-luokalle
+- [x] Recursive Backtracker -algoritmi
+- [x] Yksikkötestit uudelle pinorakenteelle
+
+Tulin siihen lopputulokseen, että en aio lisätä projektiin reitinhakualgoritmeja ollenkaan, vaan lisään vielä muutamia algoritmeja labyrintin generointia varten ainoastaan.
+Tällä viikolla ei myöskään ilmennyt mitään isompia ongelmia projektin suhteen.
+
+Käyttöliittymän koodi on hieman sekavaa, mutta en oikein tiedä voiko sitä hirveästi parantaa
